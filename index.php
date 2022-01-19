e895c1944d438451681df625379a3d94d4ec3535
Aleksey Kononenko <aleksey19988@gmail.com>
2022-01-19 11:07:19 +0300

    Реализовал шаблон и структуру проекта. Добавил composer, папку с иконками, а также структуру файлов для добавления новых данных в таблицу

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Медальный зачёт</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="mt-5 mb-5">Медальный зачёт</h1>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Место</th>
+                    <th scope="col">Страна</th>
+                    <th scope="col"><img src="icons/medals/medal_1.png" alt="1 место" width="40" height="40"></th>
+                    <th scope="col"><img src="icons/medals/medal_2.png" alt="2 место" width="40" height="40"></th>
+                    <th scope="col"><img src="icons/medals/medal_3.png" alt="3 место" width="40" height="40"></th>
+                    <th scope="col">Сумма медалей
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+    <div class="container">
+        <a class="list-group">
+            <a href="#">
+                <button type="button" class="list-group-item list-group-item-action" aria-current="true">
+                    <p>Добавить страну</p>
+                </button>
+            </a>
+            <a href="#">
+                <button type="button" class="list-group-item list-group-item-action" aria-current="true">
+                    <p>Добавить медаль</p>
+                </button>
+            </a>
+            <a href="#">
+                <button type="button" class="list-group-item list-group-item-action" aria-current="true">
+                    <p>Добавить вид спорта</p>
+                </button>
+            </a>
+            <a href="#">
+                <button type="button" class="list-group-item list-group-item-action" aria-current="true">
+                    <p>Добавить спортсмена</p>
+                </button>
+            </a>
+        </div>
+    </div>
+</body>
+</html>
