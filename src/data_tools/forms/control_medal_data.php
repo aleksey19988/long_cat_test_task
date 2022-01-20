2be079b44c3c7551b3ce41fd0cb2543817186135
Aleksey Kononenko <aleksey19988@gmail.com>
2022-01-20 17:55:22 +0300

    Реализовал визуальное добавление всех данных по медалям, создал таблицу связей в таблице медалей (id страны, id спортсмена, id вида спорта). В процессе реализация всех этих данных в БД

--- src/data_tools/forms/control_medal_data.php
+++ src/data_tools/forms/control_medal_data.php
@@ -1,5 +1,12 @@
 <?php
 require_once '../forms_handlers/control_medal_handler.php';
+require_once '../../db_config/db_connect.php';
+
+$medalTypeList = $conn->query("SELECT * FROM medal_types ORDER BY id")->fetchAll();
+$countriesList = $conn->query("SELECT * FROM countries ORDER BY id")->fetchAll();
+$sportsList = $conn->query("SELECT * FROM sports ORDER BY id")->fetchAll();
+$athletesList = $conn->query("SELECT * FROM athletes ORDER BY id")->fetchAll();
+
 ?>
 
 
@@ -18,30 +25,94 @@ require_once '../forms_handlers/control_medal_handler.php';
     </div>
 </nav>
 <div class="container">
-    <h2 class="mt-5 mb-5">Добавить страну</h2>
-    <form action="../forms_handlers/control_country_handler.php" method="post">
-        <div class="mb-3">
-            <label for="add-country" class="form-label">Название страны</label>
-            <input type="text" class="form-control" id="add-country" name="country" required>
+    <h2 class="mt-5 mb-5">Добавить медаль</h2>
+    <form action="../forms_handlers/control_medal_handler.php" method="post">
+        <div class="mb-3 form-floating">
+            <select class="form-select" aria-label="medal-type" name="medal-type" id="medal-type">
+                <?php foreach($medalTypeList as $arr => $medal): ?>
+                    <option value="<?= $medal['type']; ?>"><?= $medal['type']; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <label for="medal-type">Выберите тип медали</label>
+        </div>
+        <div class="mb-3 form-floating">
+            <select class="form-select" aria-label="country" name="country" id="country">
+                <?php foreach($countriesList as $arr => $country): ?>
+                    <option value="<?= $country['name']; ?>"><?= $country['name']; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <label for="country">Выберите страну</label>
+        </div>
+        <div class="mb-3 form-floating">
+            <select class="form-select" aria-label="sport-type" name="sport-type" id="sport-type">
+                <?php foreach($sportsList as $arr => $sport): ?>
+                    <option value="<?= $sport['name']; ?>"><?= $sport['name']; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <label for="sport-type">Выберите вид спорта</label>
+        </div>
+        <div class="mb-3 form-floating">
+            <select class="form-select" aria-label="select-athlete-1" name="athlete-1" id="select-athlete-1">
+                <?php foreach($athletesList as $arr => $athlete): ?>
+                    <option value="<?= $athlete['name']; ?>"><?= $athlete['name']; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <label for="select-athlete-1">Выберите спортсмена</label>
+        </div>
+        <div class="mb-3 form-floating">
+            <select class="form-select" aria-label="select-athlete-2" name="athlete-2" id="select-athlete-2">
+                <option value="">Выберите (необязательно)</option>
+                <?php foreach($athletesList as $arr => $athlete): ?>
+                    <option value="<?= $athlete['name']; ?>"><?= $athlete['name']; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <label for="select-athlete-2">Выберите спортсмена</label>
+        </div>
+        <div class="mb-3 form-floating">
+            <select class="form-select" aria-label="select-athlete-3" name="athlete-3" id="select-athlete-3">
+                <option value="">Выберите (необязательно)</option>
+                <?php foreach($athletesList as $arr => $athlete): ?>
+                    <option value="<?= $athlete['name']; ?>"><?= $athlete['name']; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <label for="select-athlete-3">Выберите спортсмена</label>
+        </div>
+        <div class="mb-3 form-floating">
+            <select class="form-select" aria-label="select-athlete-4" name="athlete-4" id="select-athlete-4">
+                <option value="">Выберите (необязательно)</option>
+                <?php foreach($athletesList as $arr => $athlete): ?>
+                    <option value="<?= $athlete['name']; ?>"><?= $athlete['name']; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <label for="select-athlete-4">Выберите спортсмена</label>
+        </div>
+        <div class="mb-3 form-floating">
+            <select class="form-select" aria-label="select-athlete-5" name="athlete-5" id="select-athlete-5">
+                <option value="">Выберите (необязательно)</option>
+                <?php foreach($athletesList as $arr => $athlete): ?>
+                    <option value="<?= $athlete['name']; ?>"><?= $athlete['name']; ?></option>
+                <?php endforeach; ?>
+            </select>
+            <label for="select-athlete-5">Выберите спортсмена</label>
         </div>
         <button type="submit" class="btn btn-primary">Добавить</button>
     </form>
 </div>
 <div class="container mt-5">
-    <h2>Список всех стран</h2>
+    <h2>Список всех медалей</h2>
     <table class="table table-hover">
         <thead>
         <tr>
-            <th scope="col">Страна</th>
+            <th scope="col">Медаль</th>
             <th scope="col">Удалить</th>
         </tr>
         </thead>
         <tbody>
         <?php foreach($countriesList as $arr => $country): ?>
-            <tr>
-                <td value="<?= $country['name']; ?>" id="countryName"><?= $country['name']; ?></td>
-                <td id=""><button type="button" class="btn btn-outline-dark deleteBtn" onclick="getNeighbor(this)">Удалить</button></td>
-            </tr>
+<!--            <tr>-->
+<!--                <td value="--><?//= $country['name']; ?><!--" id="countryName">--><?//= $country['name']; ?><!--</td>-->
+<!--                <td id=""><button type="button" class="btn btn-outline-dark deleteBtn" onclick="getNeighbor(this)">Удалить</button></td>-->
+<!--            </tr>-->
         <?php endforeach; ?>
         </tbody>
     </table>
